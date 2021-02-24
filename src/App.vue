a70ceb29c5b3f4c5d8eb223b58af102b2fbf2a35
Laura <lauralekeu@gmail.com>
2021-02-24 09:58:23 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<!-- TEMPLATE -->
+<template>
+
+  <div id="app">
+
+  </div>
+
+</template>
+
+<!-- SCRIPT -->
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<!-- STYLE -->
+<style>
+</style>
